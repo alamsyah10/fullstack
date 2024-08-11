1e4fe603b418e0673709995e820ef72730a433a9
alamsyah10 <alamsyah_imanudin0209@gmail.com>
2024-08-11 17:52:49 +0900

    test api call

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,37 @@
 // app.js or routes.js
 const express = require('express');
+const cors = require('cors');
 const db = require('./db');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+}));
+  
 
 app.get('/', async (req, res) => {
     try {
         const result = await db.query('SELECT NOW()');
-        res.json(result.rows[0]);
+        console.log("result", result.rows[0]);
+        // res.json(result.rows[0]);
+        console.log("backend accessed", "hello world");
+        res.json({result: "Hello workd"});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+});
+
+app.get('/api', async (req, res) => {
+    try {
+        // res.json(result.rows[0]);
+        console.log("backend accessed", "hello world");
+        res.json({result: "Hello workd"});
     } catch (err) {
         console.error(err);
         res.status(500).send('Server Error');
